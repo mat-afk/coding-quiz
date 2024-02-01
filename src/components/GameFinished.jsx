54ec3ae0cd8cf25c77f6152beaf395f62f3ad0cf
Mateus <mateusc.ramos@outlook.com>
2024-01-31 23:03:11 -0300

    feat: GameFinished component and styles

--- src/components/GameFinished.jsx
+++ src/components/GameFinished.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import WellDoneIcon from "../assets/welldone.svg"
+
 import "./GameFinished.css";
 
 const GameFinished = () => {
-  return <div>GameFinished</div>;
+  return <div id="gamefinished-container">
+    <h2>Game Finished</h2>
+    <p>Final score: {5}</p>
+    <p>You got {5} questions right!</p>
+    <img src={WellDoneIcon} alt="Well done icon" />
+    <button>Restart</button>
+  </div>;
 };
 
 export default GameFinished;
